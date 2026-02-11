6f2851da7caba6b429466adc4894d010f6360de3
ASDDAWASD <aidenyang2010@gmail.com>
2026-02-11 11:06:13 -0500

    spaceship assets

--- Asteroids/asteroid.py
+++ Asteroids/asteroid.py
@@ -1,1 +1,11 @@
 import pygame
+
+class Asteroid():
+    def __init__():
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
--- Asteroids/main.py
+++ Asteroids/main.py
@@ -2,3 +2,24 @@ import pygame
 import asteroid
 import player
 
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+SCREEN_SIZE = (720, 720)
+
+#pygame screen initialization
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+running = True
+points = 0
+
+while running:
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+
+
+    pygame.display.flip()
+pygame.quit()
