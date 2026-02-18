33a103727c4a778b7ac13f210e3a7ebbf13849db
Aiden Yang <aidenyang2010@gmail.com>
2026-02-18 15:10:38 -0500

    basic movement

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -5,7 +5,7 @@ import player
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-SCREEN_SIZE = (720, 720)
+SCREEN_SIZE = (800, 800)
 
 #pygame screen initialization
 pygame.init()
@@ -13,8 +13,13 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 running = True
 points = 0
 
+#intialize clock for DT
+clock=pygame.time.Clock()
+FPS = 60
+dt=clock.tick(FPS)/1000
+
 #initialize player
-player = player.Player(screen, (SCREEN_SIZE[0]/2,SCREEN_SIZE[1]/2), 0.25)
+player = player.Player(screen, (SCREEN_SIZE[0]/2,SCREEN_SIZE[1]/2), 0.4)
 
 while running:
     screen.fill(BLACK)
@@ -24,9 +29,17 @@ while running:
     keys = pygame.key.get_pressed()
 
     #move objects
+    player.move({
+        "thrust":keys[pygame.K_UP],
+        "right":keys[pygame.K_RIGHT],
+        "left":keys[pygame.K_LEFT]
+        },15,dt)
 
     #draw sprites
     player.draw()
 
     pygame.display.flip()
+
+    #clock tick
+    dt=clock.tick(FPS)/1000
 pygame.quit()
--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.math import Vector2
+import math
 
 class Player():
     def __init__(self, screen, pos=(0,0), scale = 1):
@@ -9,17 +11,28 @@ class Player():
         self.costumes[0] = pygame.transform.scale_by(self.costumes[0], scale)
         self.costumes[1] = pygame.transform.scale_by(self.costumes[1], scale)
         self.size = (self.costumes[0].get_size())
-        self.pos = pygame.math.Vector2(pos)
-        self.vel = pygame.math.Vector2(0,0)
-        self.accel = pygame.math.Vector2(0,0)
+        self.pos = Vector2(pos)
+        self.vel = Vector2(0,-1)
+        self.accel = Vector2(0,0)
         self.screen = screen
         
 
-    def move(self):
-        pass
-
+    def move(self,controls={"thrust":False, "right":False, "left":False},speed=10,dt=0):
+        self.accel*=0
+        self.vel*=0.95
+        unit=Vector2.normalize(self.vel)*dt*speed
+        if controls["thrust"]:
+            self.accel=unit
+        if controls["right"]:
+            self.accel+=unit.rotate(90)
+        if controls["left"]:
+            self.accel+=unit.rotate(-90)
+        self.vel+=self.accel
+        self.pos+=self.vel
+    
     def draw(self):
+        dir=180-self.vel.as_polar()[1]
         if self.accel.magnitude() == 0.0:
-            self.screen.blit(self.costumes[0], (self.pos[0]-(self.size[0]/2),self.pos[1]-(self.size[1]/2)))
+            self.screen.blit(pygame.transform.rotate(self.costumes[0],dir), (self.pos[0]-(self.size[0]/2),self.pos[1]-(self.size[1]/2)))
         else:
-            self.screen.blit(self.costumes[1], (self.pos[0]-(self.size[0]/2),self.pos[1]-(self.size[1]/2)))
+            self.screen.blit(pygame.transform.rotate(self.costumes[1],dir), (self.pos[0]-(self.size[0]/2),self.pos[1]-(self.size[1]/2)))
